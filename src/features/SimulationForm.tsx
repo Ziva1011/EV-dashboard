f9429b4a936a201f862a2591e180c70708b9fd05
Ziva1011 <catarina.vi.pires@gmail.com>
2025-12-12 23:23:45 +0100

    reduced repeated code, added onBlur

--- src/features/SimulationForm.tsx
+++ src/features/SimulationForm.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import type { Props } from "recharts/types/container/Surface";
 import { useState } from "react";
 import Button from "../components/Button";
-
-interface SimulationInputs {
-  numChargePoints: number;
-  arrivalMultiplier: number; // as percentage, e.g. 100
-  carConsumption: number; // kWh
-  chargingPower: number; // kW
-}
+import type { SimulationInputs } from "../App";
 
 interface SimulationFormProps {
   onSubmit: (inputs: SimulationInputs) => void;
@@ -25,89 +19,81 @@ const SimulationForm: React.FC<SimulationFormProps> = ({
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log(inputs);
     onSubmit(inputs);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.id, e.target.value);
-    setInputs({ ...inputs, [e.target.id]: Number(e.target.value) });
+    const { id, value } = e.target;
+    setInputs({ ...inputs, [id]: value });
   };
+
+  const handleBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setInputs({ ...inputs, [id]: value === "" ? 0 : Number(value) });
+  };
+
+  const inputFields = [
+    {
+      id: "numChargePoints",
+      label: "Number of charge points",
+      suffix: "",
+      min: 0,
+      max: 20,
+    },
+    {
+      id: "arrivalMultiplier",
+      label: "Arrival probability",
+      suffix: "%",
+      min: 20,
+      max: 200,
+    },
+    {
+      id: "carConsumption",
+      label: "Average Car Consumption ",
+      suffix: "kWh",
+    },
+    {
+      id: "chargingPower",
+      label: "Charging Power per Chargepoint (kW)",
+      suffix: "kW",
+    },
+  ] as const;
 
   return (
     <>
       <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
-        <div className="mb-5">
-          <label
-            htmlFor="numChargePoints"
-            className="block mb-2.5 text-sm font-medium text-heading"
-          >
-            Number of charge points
-          </label>
-          <input
-            type="number"
-            id="numChargePoints"
-            min="0"
-            max="20"
-            value={inputs.numChargePoints}
-            onChange={handleChange}
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-            required
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="arrivalMultiplier"
-            className="block mb-2.5 text-sm font-medium text-heading"
-          >
-            Arrival probability
-          </label>
-          <input
-            type="number"
-            id="arrivalMultiplier"
-            value={inputs.arrivalMultiplier}
-            onChange={handleChange}
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-            required
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="carConsumption"
-            className="block mb-2.5 text-sm font-medium text-heading"
-          >
-            Average Car Consumption (kW)
-          </label>
-          <input
-            type="number"
-            id="carConsumption"
-            value={inputs.carConsumption}
-            onChange={handleChange}
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-            required
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="chargingPower"
-            className="block mb-2.5 text-sm font-medium text-heading"
-          >
-            Charging Power
-          </label>
-          <input
-            type="number"
-            id="chargingPower"
-            value={inputs.chargingPower}
-            onChange={handleChange}
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-            required
-          />
-        </div>
+        {inputFields.map((input) => {
+          const value = inputs[input.id as keyof SimulationInputs];
+          return (
+            <div className="mb-5 relative">
+              <label
+                htmlFor={input.id}
+                className="block mb-2.5 text-sm font-medium text-heading "
+              >
+                {input.label}
+              </label>
+              <div className="flex items-center rounded-base bg-neutral-secondary-medium border border-default-medium px-3 py-2.5 shadow-xs focus-within:ring-2 focus-within:ring-brand focus-within:border-brand">
+                <input
+                  type="number"
+                  id={input.id}
+                  value={value}
+                  //min and max missing
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className="flex-1 bg-transparent border-0 focus:ring-0 text-heading text-sm md:text-base outline-none"
+                  required
+                />
+                <span className="ml-2 text-gray-500 font-medium">
+                  {input.suffix}
+                </span>
+              </div>
+            </div>
+          );
+        })}
+
         <div className="flex flex-row">
-          <Button color="gray" onClick={onClose}>
-            Close
-          </Button>
-          <Button onClick={() => onSubmit(inputs)}>Submit</Button>
+          <Button onClick={onClose}>Close</Button>
+          <Button type="submit">Submit</Button>
         </div>
       </form>
     </>

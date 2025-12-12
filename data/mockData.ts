1e66d3edc9a568f940edd5004882532a25549a29
Ziva1011 <catarina.vi.pires@gmail.com>
2025-12-12 19:42:28 +0100

    mock data

---
+++ data/mockData.ts
@@ -0,0 +1,24 @@
+
+//Number of cars charging per hour (24h)
+export const mockHourlyCars = [
+  0, 0, 0, 0, 0, 2, 5, 8, 12, 15, 18, 20,
+  22, 20, 18, 15, 12, 8, 5, 3, 1, 0, 0, 0 
+];
+
+//Number of cars per day of the week (7 days)
+export const mockWeeklyCars = [
+  120, 130, 110, 140, 150, 160, 145
+];
+
+//Number of cars per day in a month (30 days)
+export const mockMonthlyCars = [
+ 120, 125, 130, 128, 140, 145, 138, 132, 130, 135,
+  140, 142, 145, 138, 132, 130, 128, 125, 120, 130,
+  135, 140, 142, 145, 138, 132, 130, 128, 125, 120
+];
+
+//Number of cars per month in a year (12 months)
+export const mockYearlyCars = [
+  3500, 3800, 4000, 4200, 4500, 4700,
+  4600, 4400, 4300, 4200, 4000, 3800
+];

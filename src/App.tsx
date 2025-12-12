9cd9f1808fa55f0f5b896869ff353682fe20334c
Ziva1011 <catarina.vi.pires@gmail.com>
2025-12-12 19:44:49 +0100

    add components to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Button from "./components/Button";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import SimulationForm from "./features/SimulationForm";
+import Modal from "./components/Modal";
+import SimulationChart from "./features/Charts";
+
+interface SimulationInputs {
+  numChargePoints: number;
+  arrivalMultiplier: number;
+  carConsumption: number;
+  chargingPower: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false);
+  const [inputs, setInputs] = useState<SimulationInputs>({
+    numChargePoints: 20,
+    arrivalMultiplier: 100,
+    carConsumption: 18,
+    chargingPower: 11,
+  });
+
+  const handleSubmit = (inputs: any) => {
+    console.log("Form submitted:", inputs);
+    setOpen(false);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container mx-auto mt-8 space-y-6">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
+          <h1 className="text-2xl font-bold mb-4 md:mb-0">Analytics</h1>
+
+          {/* Toggle */}
+          <div className="flex space-x-2">
+            <button className="px-4 py-2 rounded-md bg-white border border-gray-300 text-gray-700 hover:bg-gray-100 font-medium">
+              Year
+            </button>
+            <button className="px-4 py-2 rounded-md bg-white border border-gray-300 text-gray-700 hover:bg-gray-100 font-medium">
+              Month
+            </button>
+            <button className="px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 font-medium">
+              Week
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+          <Card title="First">gibberish</Card>
+          <Card title="Second">gibberish</Card>
+
+          <Card title="Third">gibberish</Card>
+          <div className="flex justify-center items-center border border-gray-300 rounded-md shadow p-4">
+            <Button onClick={() => setOpen(true)}>Open Simulation Form</Button>
+          </div>
+        </div>
+
+        <div className="bg-white rounded-xl shadow p-6">
+          <h2 className="text-lg font-semibold mb-4">Cars per day</h2>
+          <SimulationChart />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Modal isOpen={open} onClose={() => setOpen(false)}>
+        <h2 className="text-xl font-semibold mb-4">Simulation Settings</h2>
+
+        <SimulationForm simulationInputs={inputs} onSubmit={handleSubmit} />
+      </Modal>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
